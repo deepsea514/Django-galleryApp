63442db63cbf324169c7d484536a8c763709275c
dhruv <dhruvpatel7910@gmail.com>
2021-01-31 18:28:26 +0530

    1

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
 
   path('index', index, name='index'),
   path('gallery', gallery, name='gallery'),
   path('detail', product_detail, name='product-detail')
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
