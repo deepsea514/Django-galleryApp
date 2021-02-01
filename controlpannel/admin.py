312d792536a3d920017859f1027ef14c564c0744
dhruv <dhruvpatel7910@gmail.com>
2021-02-01 16:06:36 +0530

    2

--- controlpannel/admin.py
+++ controlpannel/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Comments
+from .models import Product, Comments, Categories
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Comments)
+admin.site.register(Categories)
--- controlpannel/models.py
+++ controlpannel/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from authorization.models import User
+from datetime import datetime
 # Create your models here.
+
+class Categories(models.Model):
+    category = models.CharField(primary_key=True, max_length=100)
+
 class Product(models.Model):
     P_id = models.AutoField(primary_key= True)
     P_image = models.ImageField(upload_to='products/')
+    P_price = models.IntegerField(default=0)
+    P_name = models.CharField(max_length=50, default='temp')
+    P_description = models.CharField(max_length=200, default='temp')
+    P_category = models.ForeignKey(Categories, on_delete=models.CASCADE, null=True)
 
 class Comments(models.Model):
     comment = models.CharField(max_length=300)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     P_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.now(),null=True)
+
